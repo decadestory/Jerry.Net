64528e7b90a3612ad2e28a5d7a4b71e036deb86d
decadestory <728246447@qq.com>
2015-09-21 18:06:56 +0800

    自定义日志

--- Jerry.Base/Tools/Logger.cs
+++ Jerry.Base/Tools/Logger.cs
@@ -1,36 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Jerry.Base.Tools
 {
-    public static class Logger
+    public class Logger
     {
-       static string LogPath { get; set; }
+        static string LogPath { get; set; }
 
-        static Logger(){
-            LogPath = AppDomain.CurrentDomain.BaseDirectory;
-         }
-
-        public static void Info(string content,bool saveToDb=false)
+        static Logger()
         {
-
+            LogPath = AppDomain.CurrentDomain.BaseDirectory + "/Logs/";
         }
 
+        /// <summary>
+        /// 一般日志
+        /// </summary>
+        /// <param name="content">日志内容</param>
+        /// <param name="saveToDb"></param>
+        public static void Info(string content, bool saveToDb = false)
+        {
+            SaveToFile(content, 1);
+        }
+
+        /// <summary>
+        /// 警告日志
+        /// </summary>
+        /// <param name="content">日志内容</param>
+        /// <param name="saveToDb"></param>
         public static void Warn(string content, bool saveToDb = false)
         {
-
+            SaveToFile(content, 2);
         }
 
+        /// <summary>
+        /// 错误日志
+        /// </summary>
+        /// <param name="content">日志内容</param>
+        /// <param name="saveToDb"></param>
         public static void Error(string content, bool saveToDb = false)
         {
+            SaveToFile(content, 3);
+        }
+
+        /// <summary>
+        /// 致命错误
+        /// </summary>
+        /// <param name="content">日志内容</param>
+        /// <param name="saveToDb"></param>
+        public static void Fatal(string content, bool saveToDb = false)
+        {
+            SaveToFile(content, 4);
+        }
+
+        private static void SaveToFile(string content, int level)
+        {
+            var path = GetFilePath(level);
+            var method = new StackTrace(true).GetFrame(2).GetMethod();
+            var declaringType = method.DeclaringType;
+            var nspace = declaringType != null ? declaringType.Namespace : string.Empty;
+            var md = "<" + nspace + "." + method.Name + "> ";
+            using (var sw = new StreamWriter(path, true))
+            {
+                var time = "[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss" + "]");
+                sw.WriteLine(time + md + content + "\r\n");
+                sw.Flush();
+            }
 
         }
 
-        public static void Fatal(string content, bool saveToDb = false)
+        private static string GetFilePath(int level)
         {
+            var logPath = LogPath;
+            switch (level)
+            {
+                case 1:
+                    logPath += "Info/"; break;
+                case 2:
+                    logPath += "Warn/"; break;
+                case 3:
+                    logPath += "Error/"; break;
+                case 4:
+                    logPath += "Fatal/"; break;
+            }
+            if (!Directory.Exists(logPath)) Directory.CreateDirectory(logPath);
+            logPath += DateTime.Now.ToString("yyyy-MM-dd") + ".txt";
+
+            return logPath;
 
         }
     }
--- Jerry.Test/UnitTest1.cs
+++ Jerry.Test/UnitTest1.cs
@@ -3,7 +3,9 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Jerry.Base.Common;
 using Jerry.Base.Common.Exts;
 using System.Collections.Generic;
+using System.Threading;
 using Jerry.Base.Config;
+using Jerry.Base.Tools;
 
 
 namespace Jerry.Test
@@ -14,14 +16,20 @@ namespace Jerry.Test
         [TestMethod]
         public void TestMethod1()
         {
-            // var res = new ResultMsg<PageMsg<string>>();
-            //var er = new ResultMsg<List<TreeMsg>>();
+            //var res = new ResultMsg<PageMsg<string>>();
+            //  //var er = new ResultMsg<List<TreeMsg>>();
 
-            var apps = Configs.GetAppSetting("key1");
-            var cons = Configs.GetConnSetting("conn");
+            //  var apps = Configs.GetAppSetting("key1");
+            //  var cons = Configs.GetConnSetting("conn");
 
 
-            Assert.IsTrue(1==1);
+            for (var i = 0; i < 10; i++) { 
+                Logger.Info("这是一个致命错误"+i);
+            }
+
+
+
+            Assert.IsTrue(1 == 1);
 
         }
     }
