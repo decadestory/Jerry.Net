90bf91c245e13058d27ee520472243c87a536552
decadestory <728246447@qq.com>
2015-09-22 17:31:44 +0800

    生保留小数

--- Jerry.Base/Maths/Converter.cs
+++ Jerry.Base/Maths/Converter.cs
@@ -1,21 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Jerry.Base.Maths
 {
     public static class Converter
     {
-        public static decimal ToDecimal(this object val,int len=2)
+        /// <summary>
+        ///值类型保留小数
+        /// </summary>
+        /// <param name="val"></param>
+        /// <param name="len"></param>
+        /// <returns></returns>
+        public static string ToStd(this object val, int len = 2)
         {
-            var input = Convert.ToDecimal(val).ToString("f" + len);
-            return Convert.ToDecimal(input);
-             
-            Debug.WriteLine(123.00);
-
+            try
+            {
+                var output = Convert.ToDecimal(val).ToString("f" + len);
+                return output;
+            }
+            catch (Exception)
+            {
+                return val + "";
+            }
         }
     }
 }
